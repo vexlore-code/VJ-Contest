93dafc4f292595b19873fd08dac7b2cfcb22d91b
vexlore-code <jubiofficial.dev@gmail.com>
2025-07-22 12:18:08 +0600

    Initial commit

---
+++ ACM/Prefix sum, divisors, prime/A_Static_Range_Sum_Queries.cpp
@@ -0,0 +1,50 @@
+/*
+ "La Ilaha Illallah Muhammadur Rasulullah"
+ ╔═══════════════════════════════════════╗
+             ✦ v e x l o r e ✦         	   		Created	 : 22-07-2025  11:48:58
+   "Code like a poet. Debug like a pro." 		 GitHub  : https://github.com/vexlore-code
+ ╚═══════════════════════════════════════╝
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+#define int long long
+#define endl '\n'
+
+const int MOD = 1e9 + 7, INF = 1e18, N = 2e5 + 5;
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpi;
+/*
+ ═══════════════════════════════════ MAIN CODE ═══════════════════════════════════
+*/
+signed main()
+{
+    fastIO;
+    int n, q, a, b;
+    cin >> n >> q;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    while (q--)
+    {
+        cin >> a >> b;
+    }
+    int ar[a], br[b];
+    for (int i = a; i < b; i++)
+    {
+        int sum;
+        int s[sum];
+        sum = sum[i] + sum[b + 1];
+    }
+
+    return 0;
+}
